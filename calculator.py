40047d3a05c75b3e9018eebdd9ee1042fd167031
Dima-Gomel <metalland2015@yandex.ru>
2025-01-03 22:46:36 +0300

    Task2 - Реализовано деление

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-
+import traceback
 
 class Calculator:
     last_res = None
@@ -6,6 +6,16 @@ class Calculator:
     def sum(self, n1, n2):
         self.last_res = n1 + n2
         return n1 + n2
+
+    def divide(self, n1, n2):
+        try:
+            res = n1 / n2
+            self.last_res = res
+            return res
+        except:
+            traceback.print_exc()
+
+
 
     def print_last_res(self):
         print(self.last_res)
