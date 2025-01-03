d6729acf182fc2e9fd67307b6a0648f7a4c7e52d
Dima-Gomel <metalland2015@yandex.ru>
2025-01-03 22:15:31 +0300

    ТЗ

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,9 @@
 from calculator import Calculator
 
+# Задание
+# Разработчику 1 - реализовать функцию умножения
+# Разработчику 2 - пеализовать функцию деления
+
 calc = Calculator()
 calc.sum(10, 5)
 calc.print_last_res()
